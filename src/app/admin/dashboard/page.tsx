21abe4f5190d6a52a42535ea26b297093a97ef76
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-17 14:56:02 +0700

    finish all

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,4 +1,6 @@
+import { man } from '@/app/image'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
@@ -6,7 +8,35 @@ type Props = {}
 function page({ }: Props) {
     return (
         <DefaultLayout>
-            <div>page</div>
+            <div className="relative bg-secondaryGreen w-full p-3 rounded-xl overflow-hidden">
+                {/* Decorative Circles with background */}
+                <div className="absolute -top-10 -left-10 w-32 h-32  bg-white/35 opacity-30 rounded-full z-0"></div>
+                <div className="absolute top-1/2 left-10 w-16 h-16  bg-white/35 opacity-30 rounded-full z-0"></div>
+                <div className="absolute bottom-0 right-5 w-24 h-24  bg-white/35 opacity-30 rounded-full z-0"></div>
+                <div className="absolute -bottom-10 -right-10 w-40 h-40  bg-white/35 opacity-20 rounded-full z-0"></div>
+
+                {/* Main Content */}
+                <div className="relative z-10 grid grid-cols-2">
+                    <div className="flex justify-center items-center">
+                        <div>
+                            <h1 className="font-bold text-2xl text-white">Hai admin, selamat datang 👋</h1>
+                            <p className="text-sm text-white">Mari mulai memanagement perpustakaan ini</p>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center items-center">
+                        <div className="h-40">
+                            <Image className="w-full h-full" src={man} alt="dashboard" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="grid grid-cols-3">
+
+            </div>
         </DefaultLayout>
 
     )
--- src/app/image.ts
+++ src/app/image.ts
@@ -2,6 +2,8 @@ import oneLogo from '../assets/oneLogo.svg'
 import human from '../assets/human.jpg'
 import logo from '../assets/logo.svg'
 import realLogo from '../assets/logoBuku.svg'
+import man from '../assets/man.png'
+import mann_above from '../assets/man_above_book.webp'
 import warning from '../assets/warning.png'
 
-export { oneLogo, logo, human, realLogo, warning }
+export { oneLogo, logo, human, realLogo, warning, man, mann_above }
