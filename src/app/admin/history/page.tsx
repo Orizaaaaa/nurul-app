6bf4afc60cde7049e2dd9c60d283f4e04e5e60ea
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-17 22:51:00 +0700

    finish history

--- src/app/admin/history/page.tsx
+++ src/app/admin/history/page.tsx
@@ -1,12 +1,85 @@
+'use client'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
+import { getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@heroui/react';
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
+    const rows = [
+        {
+            key: "1",
+            name: "Tony Reichert",
+            role: "CEO",
+            status: "Active",
+        },
+        {
+            key: "2",
+            name: "Zoey Lang",
+            role: "Technical Lead",
+            status: "Paused",
+        },
+        {
+            key: "3",
+            name: "Jane Fisher",
+            role: "Senior Developer",
+            status: "Active",
+        },
+        {
+            key: "4",
+            name: "William Howard",
+            role: "Community Manager",
+            status: "Vacation",
+        },
+    ];
+
+    const columns = [
+        {
+            key: "name",
+            label: "NAME",
+        },
+        {
+            key: "role",
+            label: "ROLE",
+        },
+        {
+            key: "status",
+            label: "STATUS",
+        },
+    ];
     return (
         <DefaultLayout>
-            <h1>hallo</h1>
+            <div className="mt-5 mb-4">
+                <h1 className='text-xl font-bold text-primaryGreen italic mb-2' >Buku yang di kembalikan</h1>
+                <Table >
+                    <TableHeader columns={columns}>
+                        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+                    </TableHeader>
+                    <TableBody items={rows}>
+                        {(item) => (
+                            <TableRow key={item.key}>
+                                {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+
+            <div className="mt-5 mb-4">
+                <h1 className='text-xl font-bold text-primaryGreen italic mb-2' >Daftar peminjam aktif</h1>
+                <Table >
+                    <TableHeader columns={columns}>
+                        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+                    </TableHeader>
+                    <TableBody items={rows}>
+                        {(item) => (
+                            <TableRow key={item.key}>
+                                {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
         </DefaultLayout>
     )
 }
