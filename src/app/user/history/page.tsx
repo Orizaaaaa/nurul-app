ddac0f3b33078e27c9f0da8e455eccc74cc6987b
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-18 12:25:03 +0700

    shit men

--- src/app/user/history/page.tsx
+++ src/app/user/history/page.tsx
@@ -1,4 +1,7 @@
+'use client'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
+import { columns, rows } from '@/utils/helper'
+import { getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@heroui/react'
 import React from 'react'
 
 type Props = {}
@@ -6,7 +9,23 @@ type Props = {}
 const page = (props: Props) => {
     return (
         <DefaultLayout>
-            <h1>hallo</h1>
+            <div className="mt-5 mb-4">
+                <h1 className='text-xl font-bold text-primaryGreen italic mb-2' >Riwayat Peminjaman</h1>
+                <Table >
+                    <TableHeader columns={columns}>
+                        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+                    </TableHeader>
+                    <TableBody items={rows}>
+                        {(item) => (
+                            <TableRow key={item.key}>
+                                {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+
+
         </DefaultLayout>
     )
 }
