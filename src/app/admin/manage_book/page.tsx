772e54d38b6fc97e1c871af3043a0add07ba1d05
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-20 01:07:04 +0700

    finish bro

--- src/app/admin/manage_book/page.tsx
+++ src/app/admin/manage_book/page.tsx
@@ -1,19 +1,26 @@
 'use client'
-import { createBook, getAllBooks, postImage } from '@/api/method'
+import { createBook, deleteBook, getAllBooks, postImage } from '@/api/method'
 import ButtonPrimary from '@/components/elements/buttonPrimary'
 import ButtonSecondary from '@/components/elements/buttonSecondary'
 import InputForm from '@/components/elements/input/InputForm'
 import ModalDefault from '@/components/fragments/modal/modal'
+import ModalAlert from '@/components/fragments/modal/modalAlert'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import { useDisclosure } from '@heroui/react'
 import { title } from 'process'
 import React, { useEffect, useMemo } from 'react'
 import toast from 'react-hot-toast'
+import { FaTrash } from 'react-icons/fa6'
+import { IoMdTrash } from 'react-icons/io'
 import { IoCameraOutline } from 'react-icons/io5'
+import { RiEdit2Fill } from 'react-icons/ri'
 
 type Props = {}
 
 const page = (props: Props) => {
+    const [idBook, setIdBook] = React.useState('');
+    const { isOpen: isWarningOpen, onOpen: onWarningOpen, onClose: onWarningClose } = useDisclosure();
+    const { isOpen: isUpdateOpen, onOpen: onUpdateOpen, onClose: onUpdateClose } = useDisclosure();
     const { onOpen, onClose, isOpen } = useDisclosure();
     const [data, setData]: any = React.useState([])
     const [form, setForm] = React.useState({
@@ -25,16 +32,25 @@ const page = (props: Props) => {
         price: 0,
     })
 
-    useEffect(() => {
-        const fetchBooks = async () => {
-            try {
-                const books = await getAllBooks();
-                setData(books);
-            } catch (err) {
-                console.error('Gagal mengambil data buku:', err);
-            }
-        };
+    const [formUpdate, setFormUpdate] = React.useState({
+        image: null as File | any,
+        title: '',
+        author: '',
+        stock: 0,
+        rak: '',
+        price: 0,
+    })
 
+    const fetchBooks = async () => {
+        try {
+            const books = await getAllBooks();
+            setData(books);
+        } catch (err) {
+            console.error('Gagal mengambil data buku:', err);
+        }
+    };
+
+    useEffect(() => {
         fetchBooks();
     }, []);
 
@@ -45,21 +61,26 @@ const page = (props: Props) => {
 
     //input gambar
     const handleFileManager = (fileName: string) => {
-        if (fileName === 'add') {
-            const fileInput = document.getElementById("image-input-add") as HTMLInputElement | null;
-            fileInput ? fileInput.click() : null;
+        const inputId = fileName === 'add' ? "image-input-add" : fileName === 'update' ? "image-input-update" : null;
+
+        if (inputId) {
+            const fileInput = document.getElementById(inputId) as HTMLInputElement | null;
+            fileInput?.click();
         } else {
             console.log('error');
-
         }
     };
+
     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>, InputSelect: string) => {
         if (InputSelect === 'add') {
             const selectedImage = e.target.files?.[0];
             setForm({ ...form, image: selectedImage || null });
-        } else {
+        } else if (InputSelect === 'update') {
+            const selectedImage = e.target.files?.[0];
+            setFormUpdate({ ...formUpdate, image: selectedImage || null });
+        }
+        else {
             console.log('error');
-
         }
     };
 
@@ -115,7 +136,32 @@ const page = (props: Props) => {
         }
     };
 
+    const openModalDelete = (docId: string) => {
+        onWarningOpen();
+        setIdBook(docId);
+    }
+
+    const onOpenModalUpdate = (item: any) => {
+        onUpdateOpen();
+        setFormUpdate(item);
+    }
+
+    const handleDelete = async () => {
+        try {
+            // Tampilkan toast loading saat proses delete
+            const toastId = toast.loading('Menghapus buku...');
+            // Panggil fungsi deleteBook
+            await deleteBook(idBook);
+            fetchBooks();
+            toast.success('Buku berhasil dihapus', { id: toastId });
+
+        } catch (err) {
+            console.error('Gagal menghapus buku:', err);
+            toast.error('Gagal menghapus buku!');
+        }
+    }
     console.log(data);
+    console.log('ini adalah data form update', formUpdate);
 
 
     return (
@@ -127,7 +173,7 @@ const page = (props: Props) => {
 
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
                 {data.map((item: any) => (
-                    <div className='shadow-xl rounded-lg' >
+                    <div key={item.id} className='shadow-xl rounded-lg' >
                         <div className=" flex justify-center items-center">
                             <img className=' rounded-t-lg h-40 w-full' src={item.image} alt="" />
                         </div>
@@ -136,13 +182,120 @@ const page = (props: Props) => {
                             <h1 className='text-sm font-semibold'>Seri Mengelola Negara Ketika Sarah Marah</h1>
                             <h1 className='text-sm ' >{item.stock}</h1>
                             <h1 className='text-sm text-gray-400' >Rak nomor {item.rak}</h1>
+                            <div className="flex justify-end gap-2 mt-1  ">
+                                <RiEdit2Fill onClick={() => onOpenModalUpdate(item)} className='cursor-pointer' size={20} color='#3E5F44' />
+                                <IoMdTrash onClick={() => openModalDelete(item.id)} className='cursor-pointer' size={20} color='red' />
+                            </div>
                         </div>
+
                     </div>
                 ))}
             </div>
 
-            <ModalDefault isOpen={isOpen} onClose={onClose}>
+            <ModalDefault isOpen={isUpdateOpen} onClose={onUpdateClose}>
 
+                <h1 className='text-xl font-medium mb-4' >Edit Buku</h1>
+                <div className="images">
+
+                    {formUpdate.image && formUpdate.image instanceof Blob ? (
+                        <img
+                            className="h-[20px] md:h-[100px] w-[70px] mx-auto rounded-md object-cover"
+                            src={URL.createObjectURL(formUpdate.image)}
+                        />
+                    ) : (
+                        <img
+                            className="h-[20px] md:h-[100px] w-[70px] mx-auto rounded-md object-cover"
+                            src={formUpdate.image}
+                        />
+                    )}
+
+
+                    <input
+                        type="file"
+                        className="hidden"
+                        id="image-input-update"
+                        onChange={(e) => handleImageChange(e, 'update')}
+                    />
+
+                    <div className="flex justify-center items-center gap-3 my-3">
+                        <button className={`border-2 text-sm border-primaryGreen
+                             text-primaryGreen px-2 py-1 rounded-md 
+                             ${formUpdate.image === null ? 'hidden' : ''}`}
+                            type="button" onClick={() => handleFileManager('update')}>
+                            Ubah Gambar
+                        </button>
+                    </div>
+                </div>
+                <InputForm
+                    className="bg-green-700/30"
+                    type="text"
+                    value={formUpdate.title}
+                    marginDiown='mb-0'
+                    htmlFor="title"
+                    title="Judul Buku"
+                    placeholder="Masukan Judul Buku"
+                    onChange={handleChange}
+                />
+
+                <InputForm
+                    className="bg-green-700/30"
+                    type="text"
+                    value={formUpdate.author}
+                    marginDiown='mb-0'
+                    htmlFor="author"
+                    title="Penulis Buku"
+                    placeholder="Masukan Penulis Buku"
+                    onChange={handleChange}
+                />
+
+                <div className="flex gap-5">
+                    <InputForm
+                        className="bg-green-700/30"
+                        type="number"
+                        value={formUpdate.stock}
+                        marginDiown='mb-0'
+                        htmlFor="stock"
+                        title="Stok Buku"
+                        placeholder="Masukan Jumlah Stok"
+                        onChange={handleChange}
+                    />
+
+                    <InputForm
+                        className="bg-green-700/30"
+                        type="text"
+                        value={formUpdate.price}
+                        marginDiown='mb-0'
+                        htmlFor="price"
+                        title="Harga Buku"
+                        placeholder="Masukan Harga Buku"
+                        onChange={handleChange}
+                    />
+
+                </div>
+
+                <InputForm
+                    className="bg-green-700/30"
+                    type="text"
+                    value={formUpdate.rak}
+                    marginDiown='mb-0'
+                    htmlFor="rak"
+                    title="Lokasi Rak"
+                    placeholder="Masukan Lokasi Rak"
+                    onChange={handleChange}
+                />
+
+
+
+                <div className="flex justify-end">
+                    <div className="flex">
+                        <ButtonSecondary className='py-2 px-3 rounded-xl mr-2'  >Batal</ButtonSecondary>
+                        <ButtonPrimary className='py-2 px-3 rounded-xl' >Tambah</ButtonPrimary>
+                    </div>
+                </div>
+
+            </ModalDefault>
+
+            <ModalDefault isOpen={isOpen} onClose={onClose} >
                 <h1 className='text-xl font-medium mb-4' >Tambah Buku</h1>
                 <div className="images">
 
@@ -243,8 +396,15 @@ const page = (props: Props) => {
                         <ButtonPrimary className='py-2 px-3 rounded-xl' onClick={handleSubmit} >Tambah</ButtonPrimary>
                     </div>
                 </div>
-
             </ModalDefault>
+
+            <ModalAlert isOpen={isWarningOpen} onClose={onWarningClose} >
+                <h1>Apakah anda yakin akan menghapus buku ini ?</h1>
+                <div className="flex justify-end gap-2">
+                    <ButtonSecondary className='py-1 px-2 rounded-xl ' onClick={onWarningClose} >Batal</ButtonSecondary>
+                    <ButtonPrimary className='py-1 px-2 rounded-xl' onClick={handleDelete}  >Hapus</ButtonPrimary>
+                </div>
+            </ModalAlert>
         </DefaultLayout>
     )
 }
